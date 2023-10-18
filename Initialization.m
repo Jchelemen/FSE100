8a38b9f7a494821145dcd334c609211892b546a3
Jchelemen <148381696+Jchelemen@users.noreply.github.com>
2023-10-18 16:32:35 -0700

    Add files via upload

---
+++ Initialization.m
@@ -0,0 +1,5 @@
+%%MATLAB INITIALIZATION 
+
+javaclasspath("C:\Program Files\MATLAB\R2023b\toolbox\EV3_Toolbox\EV3");
+brick = Brick('ioType','wifi','wfAddr','127.0.0.1','wfPort',5555,'wfSN','0016533dbaf5')
+
